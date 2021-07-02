1dabb5a84374f2cf8bd7aaecb6fdaae8227b1de6
sshajeeth <sshajeeth@hotmail.com>
2021-07-02 11:00:36 +0530

    spring first class

---
+++ class1/src/main/java/com/sshajeeth/Class1/Class1Application.java
@@ -0,0 +1,13 @@
+package com.sshajeeth.Class1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class1Application.class, args);
+	}
+
+}
